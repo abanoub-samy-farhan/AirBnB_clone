f7b146962860bdb626f19ffcc4ec62129a7eab9e
semagrb <fithisalma@gmail.com>
2023-12-09 21:33:07 -0800

    ss

---
+++ just_a_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Welcome to our AirBnB clone - The console project
+"""
+
+def welcome_message():
+    """Prints the welcome message"""
+    print("Welcome to our AirBnB clone - The console project")
+
+if __name__ == "__main__":
+    welcome_message()
