7ed17bb96e5a8ed299706c9e4a5e3f0eac057768
BeboNewEra <abanoubsamy2341@gmail.com>
2023-12-11 01:47:08 +0200

    edits

--- models/user.py
+++ models/user.py
@@ -5,11 +5,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class"""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-
     def __init__(self, *args, **kwargs):
         """Initialize User"""
         super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
